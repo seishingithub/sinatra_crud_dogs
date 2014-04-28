9dc113c946f8db79e997ca15cf32265e56bff49c
Peggy Griffin <seishingithub@gmail.com>
2014-04-27 23:30:36 -0600

    User can create dogs

--- application.rb
+++ application.rb
@@ -14,4 +14,19 @@ class Application < Sinatra::Application
   get '/' do
     erb :index
   end
+
+  get '/dogs' do
+    erb :dogs, locals: {:dogs => DB[:dogs].all}
+  end
+
+  get '/dogs/new' do
+    erb :new_dogs
+  end
+
+  post '/dogs' do
+    DB[:dogs].insert({:name => params[:name], :description => params[:description], :rating_1_to_5 => params[:rating_1_to_5]})
+    redirect '/dogs'
+  end
+
+
 end
--- spec/features/application_spec.rb
+++ spec/features/application_spec.rb
@@ -4,11 +4,23 @@ require 'capybara/rspec'
 Capybara.app = Application
 
 feature 'Homepage' do
-  scenario 'Shows the welcome message' do
+  scenario 'Creating dogs' do
     visit '/'
-
-    expect(page).to have_content 'Welcome!'
+    click_on 'Dogs'
+    click_on 'Add Dog'
+    fill_in 'name', with: 'poodle'
+    fill_in 'description', with: 'intelligent'
+    fill_in 'rating_1_to_5', with: 5
+    click_on 'Create Dog'
+    expect(page).to have_content 'poodle'
   end
-end
 
 
+  end
+
+
+=begin
+      String :name, :null=>false
+      String :description, :null=>false
+      Integer :rating_1_to_5
+=end
